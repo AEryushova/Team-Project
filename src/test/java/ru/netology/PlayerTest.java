c48add4c34722ef1204b8ac5cf491c53f1c8de0b
Neto test <lsorlova82@gmail.com>
2023-02-07 10:43:01 +0200

    new tests were added to PlayerTest

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,6 +1,8 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
@@ -18,6 +20,54 @@ public class PlayerTest {
         int actual = player.sumGenre(game.getGenre());
         assertEquals(expected, actual);
     }
+    @Test
+    public void shouldInstallGame() {
+        GameStore store = new GameStore();
+        Player player = new Player("Petya");
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
-    // другие ваши тесты
+        int expected = 0;
+        int actual = playedTime.put(game, 0);;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPlayReturnRunTimeException() {
+        GameStore store = new GameStore();
+        Player player = new Player("Petya");
+
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            playedTime.get(game);
+        });
+    }
+
+    @Test
+    public void shouldPlay() {
+        GameStore store = new GameStore();
+        Player player = new Player("Petya");
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        game1.getStore().addPlayTime(game, 10);
+
+        int expected = 10;
+        int actual = playedTime.put(game1, playedTime.get(game1));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowMostPlayerByGenre() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.play(game, 3);
+
+        String expected = "Petya";
+        String actual = String.valueOf(player.sumGenre(game.getGenre()));
+        assertEquals(expected, actual);
+
+    }
+
 }
