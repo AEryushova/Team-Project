2d1fdd24c700b97e0686d303a11685a95faa1a81
Anna Eryushova <eryushovaa@yandex.ru>
2023-02-10 20:15:28 +0400

    Tets change

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -10,63 +10,38 @@ import java.util.Map;
 
 public class GameStoreTest {
 
-
-    /* добавление игры в лист + + какой ассерт используется для сравнения словарей?*/
     @Test
-    public void shouldAddGame() {
+    public void shouldAddGameThatTheGameIsNotInTheStore() {
         GameStore store = new GameStore();
         Game game1 = store.publishGame("Покер", "Карточные");
-        List<Game> expected = ;
-        List<Game> actual = store.getGames();
-        Assertions.assertLinesMatch(expected, actual);
-    }
-
-    /* проверка наличия игры в каталоге, если ее там нет */
-    @Test
-    public void shouldCheckThatTheGameIsNotInTheStore() {
-        GameStore store = new GameStore();
-        Game game1 = store.publishGame("Покер", "Карточные");
-        boolean expected = false;
+        boolean expected = true;
         boolean actual = store.containsGame(game1);
         Assertions.assertEquals(expected, actual);
     }
-
-    /* проверка наличия игры в каталоге, если она там уже есть (изменить т.к выкидывает исключение)*/
     @Test
-    public void shouldCheckThatTheGameInTheStore() {
+    public void shouldNoAddGameThatTheGameInTheStore() {
         GameStore store = new GameStore();
         Game game1 = store.publishGame("Покер", "Карточные");
         Game game2 = store.publishGame("Нарды", "Настольные");
-        Game game3 = store.publishGame("Покер", "Карточные");
-        boolean expected = true;
-        boolean actual = store.containsGame(game2);
+        Assertions.assertThrows(RuntimeException.class, () -> {store.publishGame("Покер", "Карточные");});
+    }
+    @Test
+    public void shouldRegisteredTimeAndAddPlayerAndSumPlayTime() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Покер", "Карточные");
+        int expected = 2;
+        int actual = store.addPlayTime("Kolya", 2);
         Assertions.assertEquals(expected, actual);
     }
-    /* регистрация количества времени,которое игрок провел за игрой, если игрока нет в мапе
-    + какой ассерт используется для сравнения словарей?*/
-
     @Test
-    public void shouldAddPlayerAndPlayTime() {
-        GameStore store = new GameStore();
-        store.addPlayTime("Kolya", 2);
-        Map<String, Integer> expected = ("Kolya", 2);
-        Map<String, Integer> actual = store.getPlayedTime();
-        Assertions. assert
-    }
-
-    /* регистрация количества времени,которое игрок провел за игрой, если игрок уже есть в мапе
-     + какой ассерт используется для сравнения словарей?*/
-    @Test
-    public void shouldRecordTheTimeThatThePlayerSpentInTheGame() {
+    public void shouldRegisteredTimeAndSumPlayTime() {
         GameStore store = new GameStore();
         store.addPlayTime("Kolya", 2);
         store.addPlayTime("Kolya", 3);
-        Map<String, Integer> expected = ("Kolya", 3);
-        Map<String, Integer> actual = store.getPlayedTime();
-        Assertions. assert
+        int expected = 9;
+        int actual = store.addPlayTime("Kolya", 4);
+        Assertions.assertEquals(expected, actual);
     }
-
-    /* поиск имени игрока, который играл в игры этого каталога больше всего времени*/
     @Test
     public void shouldFindThePlayerWhoPlayedTheMostTime() {
         GameStore store = new GameStore();
@@ -77,8 +52,14 @@ public class GameStoreTest {
         String actual = store.getMostPlayer();
         Assertions.assertEquals(expected, actual);
     }
-
-    /* поиск имени игрока, который играл в игры этого каталога больше всего времени, если игроков вообще нет*/
+    @Test
+    public void shouldFindThePlayerWhoPlayedTheMostTimeIfThereAreOnePlayers() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Kolya", 2);
+        String expected = "Kolya";
+        String actual = store.getMostPlayer();
+        Assertions.assertEquals(expected, actual);
+    }
     @Test
     public void shouldFindThePlayerWhoPlayedTheMostTimeIfThereAreNoPlayers() {
         GameStore store = new GameStore();
@@ -86,8 +67,6 @@ public class GameStoreTest {
         String actual = store.getMostPlayer();
         Assertions.assertEquals(expected, actual);
     }
-
-    /* суммирование общего количества времени всех игроков*/
     @Test
     public void mustSumUpTheTotalAmountOfTimeOfAllPlayers() {
         GameStore store = new GameStore();
@@ -95,6 +74,21 @@ public class GameStoreTest {
         store.addPlayTime("Masha", 5);
         store.addPlayTime("Lena", 4);
         int expected = 11;
+        int actual = store.getSumPlayedTime();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void mustSumUpTheTotalAmountOfTimeOfAllPlayersIfThereAreOnePlayers() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Masha", 5);
+        int expected = 5;
+        int actual = store.getSumPlayedTime();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void mustSumUpTheTotalAmountOfTimeOfAllPlayersIfThereAreNoPlayers() {
+        GameStore store = new GameStore();
+        int expected = 0;
         int actual = store.getSumPlayedTime();
         Assertions.assertEquals(expected, actual);
     }
